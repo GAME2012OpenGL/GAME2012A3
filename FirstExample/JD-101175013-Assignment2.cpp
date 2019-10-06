e9763c22af904252b7c9f8ad0038a8e3c596c473
Teurowel <47538462+Teurowel@users.noreply.github.com>
2019-10-06 14:21:32 -0400

    GAME2012A2 Initial commit

---
+++ FirstExample/JD-101175013-Assignment2.cpp
@@ -0,0 +1,287 @@
+﻿//***************************************************************************
+// JD-101175013-Assignment2.cpp by Jang Doosung (C) 2018 All Rights Reserved.
+//
+// Assignment 2 submission.
+//
+// Description:
+// Index draw.
+// Perspective projection
+//
+//*****************************************************************************
+
+using namespace std;
+#include <iostream>
+#include "stdlib.h"
+#include "time.h"
+#include "vgl.h"
+#include "LoadShaders.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+
+#define X_AXIS glm::vec3(1,0,0)
+#define Y_AXIS glm::vec3(0,1,0)
+#define Z_AXIS glm::vec3(0,0,1)
+#define XY_AXIS glm::vec3(1,1,0)
+#define YZ_AXIS glm::vec3(0,1,1)
+#define XZ_AXIS glm::vec3(1,0,1)
+
+GLuint vao, ibo, points_vbo, colours_vbo, modelID;
+float rotAngle = 0.0f;
+
+// Horizontal and vertical ortho offsets.
+float osH = 0.0f, osV = 0.0f, scrollSpd = 0.25f;
+
+int deltaTime, currentTime, lastTime = 0;
+glm::mat4 mvp, view, projection;
+
+int WindowWidth = 800;
+int WindowHeight = 600;
+
+glm::vec3 CameraPosition = glm::vec3(0.f, 0.f, 10);
+float fCameraSpeed = 0.5f;
+
+GLshort cube_indices[] = {
+	// Front.
+	3, 2, 1, 0, 
+	// Left.
+	0, 3, 7, 4,
+	// Bottom.
+	4, 0, 1, 5,
+	// Right.
+	5, 1, 2, 6,
+	// Back.
+	6, 5, 4, 7,
+	// Top.
+	7, 6, 2, 3
+};
+
+GLfloat cube_vertices[] = {
+	-0.9f, -0.9f, 0.9f,		// 0.
+	0.9f, -0.9f, 0.9f,		// 1.
+	0.9f, 0.9f, 0.9f,		// 2.
+	-0.9f, 0.9f, 0.9f,		// 3.
+	-0.9f, -0.9f, -0.9f,	// 4.
+	0.9f, -0.9f, -0.9f,		// 5.
+	0.9f, 0.9f, -0.9f,		// 6.
+	-0.9f, 0.9f, -0.9f,		// 7.
+};
+
+GLfloat colours[] = { 
+	1.0f, 0.0f, 0.0f,		
+	0.0f, 1.0f, 0.0f, 
+	0.0f, 0.0f, 1.0f,
+	1.0f, 1.0f, 0.0f,
+	1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 1.0f,
+	0.5f, 0.0f, 0.0f,
+	0.0f, 0.5f, 0.0f
+};
+
+void init(void)
+{
+	//Specifying the name of vertex and fragment shaders.
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+	
+	//Loading and compiling shaders
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);	//My Pipeline is set up
+
+	modelID = glGetUniformLocation(program, "mvp");
+
+	// Perspective arameters : Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	// projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	// Ortho parameters: left, right, bottom, top, nearVal, farVal
+	//projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.0f, 100.0f); // In world coordinates
+	projection = glm::perspective(glm::radians(45.f), (GLfloat)WindowWidth / (GLfloat)WindowHeight, 0.1f, 100.f);
+	
+	// Camera matrix
+	view = glm::lookAt
+	(
+		CameraPosition,		// Camera pos in World Space
+		glm::vec3(0, 0, 0),		// and looks at the origin
+		glm::vec3(0, 1, 0)		// Head is up (set to 0,-1,0 to look upside-down)
+	);
+
+	vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+		points_vbo = 0;
+		glGenBuffers(1, &points_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(0);
+
+		colours_vbo = 0;
+		glGenBuffers(1, &colours_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(1);
+		//glBindBuffer(GL_ARRAY_BUFFER, 0); // Can optionally unbind the buffer to avoid modification.
+		
+	//glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+	
+	// Enable depth test.
+	glEnable(GL_DEPTH_TEST);
+}
+
+//---------------------------------------------------------------------
+//
+// transformModel
+//
+
+void transformObject(float scale, glm::vec3 rotationAxis, float rotationAngle, glm::vec3 translation) {
+	glm::mat4 Model;
+	Model = glm::mat4(1.0f);
+	Model = glm::translate(Model, translation);
+	Model = glm::rotate(Model, glm::radians(rotationAngle), rotationAxis);
+	Model = glm::scale(Model, glm::vec3(scale));
+	mvp = projection * view * Model;
+	glUniformMatrix4fv(modelID, 1, GL_FALSE, &mvp[0][0]);
+}
+
+//---------------------------------------------------------------------
+//
+// display
+//
+
+void display(void)
+{
+	// Delta time stuff.
+	currentTime = glutGet(GLUT_ELAPSED_TIME); // Gets elapsed time in milliseconds.
+	deltaTime = currentTime - lastTime;
+	lastTime = currentTime;
+
+	//Camera Update
+	view = glm::lookAt
+	(
+		CameraPosition,		// Camera pos in World Space
+		glm::vec3(0, 0, 0),		// and looks at the origin
+		glm::vec3(0, 1, 0)		// Head is up (set to 0,-1,0 to look upside-down)
+	);
+
+	glClearColor(0.0f, 1.0f, 0.0f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glBindVertexArray(vao);
+
+	//transformObject(0.4f, YZ_AXIS, rotAngle+=((float)45 / (float)1000 * deltaTime), glm::vec3(0.0f, 0.0f, 0.0f));
+	//transformObject(0.4f, YZ_AXIS, rotAngle += 5.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+
+	static float Cub1RotAngle = 0.f;
+	Cub1RotAngle += ((float)45 / (float)1000 * deltaTime);
+	if (Cub1RotAngle >= 360.f)
+		Cub1RotAngle -= 360.f;
+	transformObject(0.3f, Y_AXIS, Cub1RotAngle, glm::vec3(0.f, 0.45f, 0.f));
+
+	//Ordering the GPU to start the pipeline
+	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_SHORT, 0);
+	
+	//Second Cube
+	static float Cub2RotAngle = 0.f;
+	Cub2RotAngle -= ((float)45 / (float)1000 * deltaTime);
+	if (Cub2RotAngle <= -360.f)
+		Cub2RotAngle += 360.f;
+	transformObject(0.3f, Y_AXIS, Cub2RotAngle, glm::vec3(0.f, -0.45f, 0.f));
+
+	//Ordering the GPU to start the pipeline
+	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_SHORT, 0);
+
+	glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	glutSwapBuffers(); // Instead of double buffering.
+}
+
+void idle()
+{
+	//glutPostRedisplay();
+}
+
+void timer(int id)
+{ 
+	glutPostRedisplay();
+	glutTimerFunc(33, timer, 0); 
+}
+
+void keyDown(unsigned char key, int x, int y)
+{
+	// Orthographic.
+	switch(key)
+	{
+		case 'w':
+			CameraPosition.z -= fCameraSpeed;
+			//osV -= scrollSpd;
+			break;
+		case 's':
+			CameraPosition.z += fCameraSpeed;
+			//osV += scrollSpd;
+			break;
+		case 'a':
+			CameraPosition.x -= fCameraSpeed;
+			//osH += scrollSpd;
+			break;
+		case 'd':
+			CameraPosition.x += fCameraSpeed;
+			//osH -= scrollSpd;
+			break;
+		case 'r':
+			CameraPosition.y += fCameraSpeed;
+			break;
+		case 'f':
+			CameraPosition.y -= fCameraSpeed;
+			break;
+	}
+}
+
+void keyUp(unsigned char key, int x, int y)
+{
+	// Empty for now.
+}
+
+void mouseMove(int x, int y)
+{
+	cout << "Mouse pos: " << x << "," << y << endl;
+}
+
+void mouseDown(int btn, int state, int x, int y)
+{
+	cout << "Clicked: " << (btn == 0 ? "left " : "right ") << (state == 0 ? "down " : "up ") <<
+		"at " << x << "," << y << endl;
+}
+
+//---------------------------------------------------------------------
+//
+// main
+//
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize(WindowWidth, WindowHeight);
+	glutCreateWindow("Jang, Doosung, 101175013");
+
+	glewInit();	//Initializes the glew and prepares the drawing pipeline.
+	init();
+
+	// Set all our glut functions.
+	glutDisplayFunc(display);
+	glutIdleFunc(idle);
+	glutTimerFunc(33, timer, 0);
+	glutKeyboardFunc(keyDown);
+	glutKeyboardUpFunc(keyUp);
+	glutMouseFunc(mouseDown);
+	glutPassiveMotionFunc(mouseMove); // or...
+	//glutMotionFunc(mouseMove); // Requires click to register.
+	glutMainLoop();
+}
